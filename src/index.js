a5e7887a7dd72d84f77a546e47e9a25dd3e156a3
jeandeswesley <jeandeswesley@hotmail.com>
2022-02-03 21:29:07 -0300

    Instalação do Nodemon, statement, account e deposit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
-const { query } = require("express");
+const { query, request, response } = require("express");
 const express = require("express");
-
+const { v4: uuidv4 } = require("uuid");
+//v4 gera numeros randomicos
 const app = express();
 
 app.use(express.json());
 
+const customers = [];
+
+//Middleware
+function verifyExistsAccountCPF(request, response, next) {
+  const { cpf } = request.headers;
+
+  const customer = customers.find((customer) => customer.cpf === cpf);
+
+  if (!customer) {
+    return response.status(400).json({ error: "Customer not found" });
+  }
+
+  request.customer = customer;
+
+  return next();
+}
 /**
  * Importante:
  * GET - Buscar uma informação dentro do servidor
@@ -22,30 +39,52 @@ app.use(express.json());
  * Body Params => Os objetos inserção/alteração
  */
 
-app.get("/courses", (request, response) => {
-  const query = request.query;
-  console.log(query);
-  return response.json(["curso1", "curso2"]);
+app.post("/account", (request, response) => {
+  /**
+   * cpf - string  * nome - setring  * id - uuid  * statement []
+   */
+  const { cpf, name } = request.body;
+
+  //Verifica se existe cpf
+  const customersAlreadyExists = customers.some(
+    (customers) => customers.cpf === cpf
+  );
+
+  if (customersAlreadyExists) {
+    return response.status(400).json({ error: "Customers already exists!" });
+  }
+
+  customers.push({
+    cpf,
+    name,
+    id: uuidv4(),
+    statement: [],
+  });
+
+  return response.status(201).send();
 });
 
-app.post("/courses", (request, response) => {
-  const body = request.body;
-  console.log(body);
-  return response.json(["curso1", "curso2", "curso3"]);
+app.get("/statement/", verifyExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+  console.log(customer.statement);
+  return response.json(customer.statement);
 });
 
-app.put("/courses/:id", (request, response) => {
-  const { id } = request.params;
-  console.log(id);
-  return response.json(["curso1", "curso2", "Curso4"]);
-});
+app.post("/deposit/", verifyExistsAccountCPF, (request, response) => {
+  const { description, amount } = request.body;
 
-app.patch("/courses/:id", (request, response) => {
-  return response.json(["curso7", "curso2"]);
-});
+  const { customer } = request;
 
-app.delete("/courses/:id", (request, response) => {
-  return response.json(["curso1"]);
+  const statementOperation = {
+    description,
+    amount,
+    created_at: new Date(),
+    type: "credit",
+  };
+
+  customer.statement.push(statementOperation);
+
+  return response.status(201).send();
 });
 
 app.listen(3333);
